baa8f2a5b3828be5f7326632e396bc93fcb34445
damienfamed75 <thedoubledimensions@gmail.com>
2020-07-09 00:50:18 -0500

    move key updating in game.rs

--- src/game.rs
+++ src/game.rs
@@ -1,10 +1,12 @@
+// standard libraries
 use std::thread;
 use std::time::Duration;
-
-use crate::grid::Grid;
-
+// internal modules
+use crate::grid::{Grid, GRID_WIDTH, GRID_HEIGHT};
+// external libraries
 use device_query::{DeviceQuery, DeviceState, Keycode};
 
+// How long the update function pauses after drawing.
 const UPDATE_PAUSE_MILLIS: u64 = 500;
 
 pub struct Game {
@@ -24,9 +26,9 @@ impl Game {
     pub fn update(&mut self) -> Result<(), ()> {
         let keys: Vec<Keycode> = self.device.get_keys();
         for key in keys.iter() {
+			// Only try to move if it's a valid movement key.
             if is_move_key(key) {
-				let delta_score = self.map.mov(key);
-				self.score += delta_score;
+				self.update_movement(key);
 				self.map.new_rand_block()?;
                 self.draw();
                 println!("Pressed key: {:?}", key);
@@ -36,11 +38,44 @@ impl Game {
             }
 		}
 		Ok(())
-    }
+	}
+	pub fn update_movement(&mut self, dir: &Keycode) {
+		match *dir {
+			Keycode::Right => {
+				self.score += self.map.mov_dir(
+					(0..GRID_WIDTH).by_ref(),
+					(0..GRID_HEIGHT).rev().by_ref(),
+					0, 1,
+				);
+			},
+			Keycode::Left => {
+				self.score += self.map.mov_dir(
+					(0..GRID_WIDTH).by_ref(),
+					(0..GRID_HEIGHT).by_ref(),
+					0, -1,
+				);
+			},
+			Keycode::Up => {
+				self.score += self.map.mov_dir(
+					(0..GRID_WIDTH).by_ref(),
+					(0..GRID_HEIGHT).by_ref(),
+					-1, 0,
+				);
+			},
+			Keycode::Down => {
+				self.score += self.map.mov_dir(
+					(0..GRID_WIDTH).rev().by_ref(),
+					(0..GRID_HEIGHT).by_ref(),
+					1, 0,
+				);
+			},
+			_ => {},
+		}
+	}
     pub fn draw(&self) {
         print!("\x1B[2J\x1B[1;1H");
-        println!("score: {}", self.score);
-        println!("{}", self.map.to_string());
+        println!("score: {} | ", self.score);
+        println!("{}", self.map);
 	}
 }
 
--- src/grid.rs
+++ src/grid.rs
@@ -1,15 +1,17 @@
+// standard libraries
 use std::iter::Iterator;
-
+use std::fmt;
+use std::fmt::{Display};
+// internal modules
 use crate::object::Object;
-
-use device_query::Keycode;
+// external libraries
 use rand::Rng;
 
-const GRID_WIDTH: usize         = 4; // width
-const GRID_HEIGHT: usize        = 4; // height
-const BOX_WIDTH: usize          = 8; // individual box width
-const BLOCK_DEFAULT_NUMBER: i16 = 2; // random block starting value
-const MAX_RETRY_RAND_BLOCK: i32 = 64;
+pub const GRID_WIDTH: usize			= 4; // width
+pub const GRID_HEIGHT: usize		= 4; // height
+const BOX_WIDTH: usize				= 8; // individual box width
+const BLOCK_DEFAULT_NUMBER: i16		= 2; // random block starting value
+const MAX_RETRY_RAND_BLOCK: i32		= 64;
 
 // vertical bars │ │ │
 // separator ──────
@@ -22,6 +24,12 @@ const MAX_RETRY_RAND_BLOCK: i32 = 64;
 
 // Grid is a 4x4 2 dimensional array each containing a block.
 pub struct Grid([[Object; GRID_HEIGHT]; GRID_WIDTH]);
+
+impl Display for Grid {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.to_string())
+    }
+}
 
 impl Grid {
     pub fn new() -> Self {
@@ -50,46 +58,39 @@ impl Grid {
 			}
         }
 	}
-    pub fn to_string(&self) -> String {
-        let arr = self.0;
-        let mut response = String::new();
-		// Append the top of the grid.
-        response.push_str(&format!(
-            "┌{0:─>width$}┬{0:─>width$}┬{0:─>width$}┬{0:─>width$}┐",
-            "", // intentional empty string
-            width = BOX_WIDTH
-        ));
-
-        for x in 0..GRID_WIDTH {
-            response.push_str("\n│");
-            response.push_str(&format!("{: >width$}│", "", width = BOX_WIDTH).repeat(arr[x].len()));
-            response.push_str("\n│");
-
-            // Write the formatted object to the string.
-            for obj in arr[x].iter() {
-				// each iteration adds: "{num}   |"
-                response.push_str(&obj.fmt_width(BOX_WIDTH));
-            }
-
-            response.push_str("\n│");
-            response.push_str(&format!("{: >width$}│", "", width = BOX_WIDTH).repeat(arr[x].len()));
-            // If this isn't the last line, then draw connectors between all of
-            // the boxes.
-            if x != GRID_WIDTH - 1 {
-                response.push_str(&format!("\n├{:─>width$}┼", "─", width = BOX_WIDTH));
-                response.push_str(
-                    &format!("{:─>width$}┼", "─", width = BOX_WIDTH).repeat(arr[x].len() - 2),
-                );
-                response.push_str(&format!("{:─>width$}┤", "─", width = BOX_WIDTH));
-            }
-        }
-		// Append the bottom of the grid.
-        response.push_str(&format!(
-            "\n└{0:─>width$}┴{0:─>width$}┴{0:─>width$}┴{0:─>width$}┘",
-            "", // intentional empty string
-            width = BOX_WIDTH
-        ));
-        response
+	// mov_dir moves in a direction specified.
+	// -1 or 1 to move in that direction whether it be x or y.
+	// 0 means no movement on this axis.
+	//
+	// Pass in the iterator for the x and y axis.
+	// Iterator is passed in because movement on an axis may affect what direction
+	// you want to check the blocks in.
+	pub fn mov_dir<X: Iterator+Clone, Y: Iterator+Clone>(&mut self, x_iter: &mut X, y_iter: &mut Y, dx: i8, dy: i8) -> i32
+	where 
+		X: Iterator<Item = usize>,
+		Y: Iterator<Item = usize>
+	{
+		let mut delta_score: i32 = 0;
+		for x in x_iter.into_iter() {
+			// We must clone the iterator because the reference is used up after
+			// the first loop.
+			for y in y_iter.clone().into_iter() {
+				if let Object::Block(number) = self.0[x][y] {
+					let (mut ox,mut oy): (i8, i8) = (x as i8, y as i8);
+					let mut onum = number; // var for original number.
+					loop {
+						let (nx, ny, nnum) = self.mov_delta(onum, ox, oy, dx, dy);
+						if (nx, ny) == (ox, oy) { break; }
+						if onum != nnum { delta_score += nnum as i32; }
+						// Originals equal the new values.
+						ox = nx;
+						oy = ny;
+						onum = nnum;
+					}
+				}
+			}
+		}
+		delta_score
 	}
 	// obj_at returns an object if one exists at the provided [x,y] coordinates.
     fn obj_at(&mut self, x: usize, y: usize) -> Option<Object> {
@@ -139,66 +140,45 @@ impl Grid {
 
         (x, y, number)
 	}
-	fn mov_dir<X: Iterator+Clone, Y: Iterator+Clone>(&mut self, x_iter: &mut X, y_iter: &mut Y, dx: i8, dy: i8) -> i32
-	where 
-		X: Iterator<Item = usize>,
-		Y: Iterator<Item = usize>
-	{
-		let mut delta_score: i32 = 0;
-		for x in x_iter.into_iter() {
-			// We must clone the iterator because the reference is used up after
-			// the first loop.
-			for y in y_iter.clone().into_iter() {
-				if let Object::Block(number) = self.0[x][y] {
-					let (mut ox,mut oy): (i8, i8) = (x as i8, y as i8);
-					let mut onum = number; // var for original number.
-					loop {
-						let (nx, ny, nnum) = self.mov_delta(onum, ox, oy, dx, dy);
-						if (nx, ny) == (ox, oy) { break; }
-						if onum != nnum { delta_score += nnum as i32; }
-						// Originals equal the new values.
-						ox = nx;
-						oy = ny;
-						onum = nnum;
-					}
-				}
-			}
-		}
-		delta_score
+	fn to_string(&self) -> String {
+        let arr = self.0;
+        let mut response = String::new();
+		// Append the top of the grid.
+        response.push_str(&format!(
+            "┌{0:─>width$}┬{0:─>width$}┬{0:─>width$}┬{0:─>width$}┐",
+            "", // intentional empty string
+            width = BOX_WIDTH
+        ));
+
+        for x in 0..GRID_WIDTH {
+            response.push_str("\n│");
+            response.push_str(&format!("{: >width$}│", "", width = BOX_WIDTH).repeat(arr[x].len()));
+            response.push_str("\n│");
+
+            // Write the formatted object to the string.
+            for obj in arr[x].iter() {
+				// each iteration adds: "{num}   |"
+                response.push_str(&obj.fmt_width(BOX_WIDTH));
+            }
+
+            response.push_str("\n│");
+            response.push_str(&format!("{: >width$}│", "", width = BOX_WIDTH).repeat(arr[x].len()));
+            // If this isn't the last line, then draw connectors between all of
+            // the boxes.
+            if x != GRID_WIDTH - 1 {
+                response.push_str(&format!("\n├{:─>width$}┼", "─", width = BOX_WIDTH));
+                response.push_str(
+                    &format!("{:─>width$}┼", "─", width = BOX_WIDTH).repeat(arr[x].len() - 2),
+                );
+                response.push_str(&format!("{:─>width$}┤", "─", width = BOX_WIDTH));
+            }
+        }
+		// Append the bottom of the grid.
+        response.push_str(&format!(
+            "\n└{0:─>width$}┴{0:─>width$}┴{0:─>width$}┴{0:─>width$}┘",
+            "", // intentional empty string
+            width = BOX_WIDTH
+        ));
+        response
 	}
-    pub fn mov(&mut self, dir: &Keycode) -> i32 {
-		let mut delta_score: i32 = 0;
-		match *dir {
-			Keycode::Right => {
-				delta_score += self.mov_dir(
-					(0..GRID_WIDTH).by_ref(),
-					(0..GRID_HEIGHT).rev().by_ref(),
-					0, 1,
-				);
-			},
-			Keycode::Left => {
-				delta_score += self.mov_dir(
-					(0..GRID_WIDTH).by_ref(),
-					(0..GRID_HEIGHT).by_ref(),
-					0, -1,
-				);
-			},
-			Keycode::Up => {
-				delta_score += self.mov_dir(
-					(0..GRID_WIDTH).by_ref(),
-					(0..GRID_HEIGHT).by_ref(),
-					-1, 0,
-				);
-			},
-			Keycode::Down => {
-				delta_score += self.mov_dir(
-					(0..GRID_WIDTH).rev().by_ref(),
-					(0..GRID_HEIGHT).by_ref(),
-					1, 0,
-				);
-			},
-			_ => {},
-		}
-		delta_score
-    }
 }
